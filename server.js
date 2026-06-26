b96b4cbc53feb0debf6fab9bcce45462adce2c92
HassanSaed <150770433+HassanSaed@users.noreply.github.com>
2026-06-27 02:12:18 +0800

    Refactor Firebase initialization and update location logging

--- server.js
+++ server.js
@@ -4,33 +4,34 @@ const admin = require("firebase-admin");
 const app = express();
 app.use(express.json());
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://student-bus-tracker-e03fc-default-rtdb.firebaseio.com"
+  databaseURL: "https://student-bus-tracker-e03fc-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
 
 const db = admin.database();
 
+app.get("/", (req, res) => {
+  res.send("TTN Firebase Webhook is running");
+});
+
 app.post("/", async (req, res) => {
   try {
-
-    const location =
-      req.body.data.location_solved.location;
+    const location = req.body.data.location_solved.location;
 
     await db.ref("buses/bus-001").set({
+      id: "bus-001",
       latitude: location.latitude,
       longitude: location.longitude,
       lastUpdated: Date.now()
     });
 
-    console.log("GPS Updated!");
-
+    console.log("GPS Updated:", location);
     res.sendStatus(200);
-
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.sendStatus(500);
   }
 });
