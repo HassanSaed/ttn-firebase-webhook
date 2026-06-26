8b0cae3f1f474eb9bdbf71b4fd53530e4fcd54c6
HassanSaed <150770433+HassanSaed@users.noreply.github.com>
2026-06-27 02:48:41 +0800

    Set up Firebase admin and create webhook endpoints

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
+const express = require("express");
+const admin = require("firebase-admin");
+
+const app = express();
+app.use(express.json());
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://student-bus-tracker-e03fc-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
+
+const db = admin.database();
+
+app.get("/", (req, res) => {
+  res.send("TTN Firebase Webhook is running");
+});
+
 app.post("/", async (req, res) => {
   try {
     const location =
@@ -24,4 +43,8 @@ app.post("/", async (req, res) => {
     console.error(err);
     res.sendStatus(500);
   }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Webhook running");
 });
